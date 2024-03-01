8ae1a40e95798cd544407ce0a9d3688b285564ad
ivanMilin <ivan.milin32@gmail.com>
2024-03-01 02:38:26 +0100

    Adeed text under the YOU WON image

--- main.cpp
+++ main.cpp
@@ -3,14 +3,12 @@
 
 using namespace sf;
 
-int N = 21, M = 21;
+int N = 21;
 
 int blockSize = 32; // Adjust the size of each block
-int w = blockSize * N;
-int h = blockSize * N;
+int w = blockSize * N, h = blockSize * N;
 
-int direction = 0;
-int num = 1;
+int direction = 0, howManyFruitsPacmanHasEaten = 0;
 
 bool paused = false; // Variable to track if the game is paused or not
 
@@ -24,7 +22,6 @@ struct Fruit
     int x,y;
 }f;
 
-bool foodMatrix[21][21];
 const bool mazeMatrix[21][21] = {
         {0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0},
         {0,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0},
@@ -48,10 +45,12 @@ const bool mazeMatrix[21][21] = {
         {1,1,1,1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,0,1,1},
         {0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0}
         };
+bool foodMatrix[21][21];
 
 // This function copies values from mazeMatrix to  
 // foodMatrix and sets edge of the matrix to zero
-void copyMatrixAndModify(){
+void copyMatrixAndModify()
+{
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
            foodMatrix[i][j] = mazeMatrix[i][j]; 
@@ -85,10 +84,11 @@ void Tick()
     }
 }
 
-void initializeFruits(std::vector<Fruit>& fruits) {
+void initializeFruits(std::vector<Fruit>& fruits) 
+{
     copyMatrixAndModify();
     for(int i = 0; i < N; ++i) {
-        for(int j = 0; j < M; ++j) {
+        for(int j = 0; j < N; ++j) {
             if (foodMatrix[j][i]) {
                 fruits.push_back({i, j});
             }
@@ -97,7 +97,8 @@ void initializeFruits(std::vector<Fruit>& fruits) {
 
 }
 
-void removeEatenFruit(std::vector<Fruit>& fruits, int x, int y) {
+void removeEatenFruit(std::vector<Fruit>& fruits, int x, int y) 
+{
     auto it = std::remove_if(fruits.begin(), fruits.end(), [x, y](const Fruit& fruit) {
         return fruit.x == x && fruit.y == y;
     });
@@ -106,49 +107,55 @@ void removeEatenFruit(std::vector<Fruit>& fruits, int x, int y) {
 
 int main()
 {
+    RenderWindow window(VideoMode(w,h+2*blockSize),"Pacman Game");
+
     std::vector<Fruit> fruits;
     initializeFruits(fruits);
     
-    int number_of_fruits_on_map = number_of_fruits_on_map = fruits.size();
-    printf("Total number of fruits : %d\n", number_of_fruits_on_map);
+    int numberOfFruitsOnMap = fruits.size();
+    printf("Total number of fruits : %d\n", numberOfFruitsOnMap);
 
-    RenderWindow window(VideoMode(w,h),"Pacman Game");
+    RectangleShape block(Vector2f(blockSize, blockSize)); // Create a rectangle shape for grid and pacman
+    RectangleShape line_vertical(Vector2f(1.f, h));       // Create a vertical line
+    RectangleShape line_horisontal(Vector2f(w, 1.f));     // Create a horizontal line
+    CircleShape fruitShape(blockSize / 4.5);              // Create a circle shape for the fruit
 
-    CircleShape fruitShape(blockSize / 4.5); // Create a circle shape for the fruit
+    fruitShape.setFillColor(Color::White);
+    line_vertical.setFillColor(Color::Black);
+    line_horisontal.setFillColor(Color::Black);
 
     Texture pacmanTexture, you_wonTexture;    
     
-    if (!pacmanTexture.loadFromFile("images/pacman.png") and !you_wonTexture.loadFromFile("images/you_won.png")) {
-        return EXIT_FAILURE;
-    }
-
-    if (!you_wonTexture.loadFromFile("images/you_won.png")) {
+    if (!pacmanTexture.loadFromFile("images/pacman.png") || !you_wonTexture.loadFromFile("images/you_won.png")) {
         return EXIT_FAILURE;
     }
 
     Sprite pacmanSprite(pacmanTexture);
     Sprite you_wonSprite(you_wonTexture);
 
-    RectangleShape block(Vector2f(blockSize, blockSize)); // Create a rectangle shape for grid and pacman
-    RectangleShape line_vertical(Vector2f(1.f, h)); // Create a vertical line
-    RectangleShape line_horisontal(Vector2f(w, 1.f)); // Create a horizontal line
-    
-    Color gridColor = Color(0, 0, 139); // Color for the grid
-    Color fruitColor = Color::White; // Color for the fruit
-
-    fruitShape.setFillColor(fruitColor);
-    line_vertical.setFillColor(Color::Black);
-    line_horisontal.setFillColor(Color::Black);
-
     Font font;
     if (!font.loadFromFile("font/Arial.ttf")) {
         return EXIT_FAILURE;
     }
 
-    Text pausedText("Paused", font, 50);
-    pausedText.setFillColor(Color::White);
+    //This text is meant for printing text when 'space' button is pressed
+    Text pausedText("Paused", font, 64);
+    pausedText.setFillColor(Color::Yellow);
     pausedText.setStyle(Text::Bold);
     pausedText.setPosition(w / 2 - pausedText.getLocalBounds().width / 2, h / 2 - pausedText.getLocalBounds().height / 2);
+
+    //This text is meant for printing how many fruit pacman has eaten
+    Text currentScoreText;
+    currentScoreText.setFont(font);
+    currentScoreText.setCharacterSize(24);
+    currentScoreText.setFillColor(Color::White);
+    currentScoreText.setPosition(w / 2 - pausedText.getLocalBounds().width / 2 + blockSize, h + blockSize);
+
+    //This text is meant for printing text when game is over to tell you how to exit window
+    Text instructionToExitGame("-> Press Esc to exit <-", font, 48);
+    instructionToExitGame.setFillColor(Color::Cyan);
+    instructionToExitGame.setStyle(Text::Bold);
+    instructionToExitGame.setPosition(w / 2 - instructionToExitGame.getLocalBounds().width / 2, h);
 
     Clock clock;
     float timer = 0;
@@ -165,15 +172,18 @@ int main()
         clock.restart();
         timer += time;
         
-        if(num == 20)
+        //if(howManyFruitsPacmanHasEaten == number_of_fruits_on_map)
+        if(howManyFruitsPacmanHasEaten == 20)
         {
             window.clear();
             you_wonSprite.setPosition(w / 2 - you_wonSprite.getLocalBounds().width / 2, h / 2 - you_wonSprite.getLocalBounds().height / 2);
             window.draw(you_wonSprite);
+            window.draw(instructionToExitGame);
 
             if(e.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape))
             {
                 window.close();
+                printf("Number of eaten fruit : %d\n",howManyFruitsPacmanHasEaten);
             }
             window.display();
         }
@@ -185,7 +195,7 @@ int main()
                 if(e.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape))
                 {
                     window.close();
-                    printf("Number of fruit : %d\n",num-1);
+                    printf("Number of fruit : %d\n", howManyFruitsPacmanHasEaten);
                 }
                 if(e.type == Event::KeyPressed && e.key.code == Keyboard::Space) {
                     paused = !paused; // Toggle pause state when space is pressed
@@ -208,7 +218,7 @@ int main()
                     if (s[0].x == fruit.x && s[0].y == fruit.y) 
                     {
                         removeEatenFruit(fruits, fruit.x, fruit.y);
-                        num++; // Increment fruit count
+                        howManyFruitsPacmanHasEaten++; // Increment fruit count
                     }
                 }
             }
@@ -217,7 +227,7 @@ int main()
 
             // Draw grid
             for(int i = 0; i < N; i++) {
-                for(int j = 0; j < M; j++) {
+                for(int j = 0; j < N; j++) {
                     block.setPosition(i * blockSize, j * blockSize);
                     if (mazeMatrix[j][i]) {
                         block.setFillColor(Color::Black); // Set color to black if matrix value is one
@@ -235,7 +245,7 @@ int main()
             }
 
             // Draw horizontal lines
-            for(int j = 1; j < M; j++) {
+            for(int j = 1; j < N; j++) {
                 line_horisontal.setPosition(0, j * blockSize);
                 window.draw(line_horisontal);
             }
@@ -257,12 +267,13 @@ int main()
             {
                 window.draw(pausedText);
             }
-            window.display();
-        }
+
+            std::string fruit_eaten = std::to_string(howManyFruitsPacmanHasEaten);
+            currentScoreText.setString("Score : " + fruit_eaten);
             
-        
+            window.draw(currentScoreText);
+            window.display();
+        }   
     }
-    
     return 0;
 }
-
